ee5e783645280b28c2d3d47e8116036dc43501eb
Inhae-na <skqhd1220@naver.com>
2025-06-24 09:18:02 +0900

    250624_4th

--- naver1/editor/insert_DB.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require_once("../connect_DB.php");
-
-  $title = $_POST['title'];
-  $content = $_POST['content'];
-
-  $result = query("INSERT INTO `board_silbi` (title, content) VALUES ('{$title}', '{$content}')");
-?>
